1c68f1cc97954ba02a13e6265452ed0faa850895
ethanstrominger <32078396+ethanstrominger@users.noreply.github.com>
2018-11-19 19:35:09 -0500

    First Player 2 test case passes

--- src/game.py
+++ src/game.py
@@ -1,2 +1,8 @@
+PAPER = "Paper"
+SCISSORS = "Scissors"
+ROCK = "Rock"
+
 def get_winning_player(player1_hand, player2_hand):
-    return True
+    if player1_hand == SCISSORS and player2_hand == ROCK:
+        return 2
+    return 1
--- test/test_confirm.py
+++ test/test_confirm.py
@@ -1,9 +1,4 @@
-from src.game import get_winning_player
-
-PAPER = "Paper"
-SCISSORS = "Scissors"
-ROCK = "Rock"
-
+from src.game import get_winning_player, PAPER, SCISSORS, ROCK
 
 def test_pass():
     assert 1==1
@@ -11,5 +6,8 @@ def test_pass():
 def test_player1_wins():
     assert get_winning_player(ROCK, SCISSORS) == 1
     assert get_winning_player(SCISSORS, PAPER) == 1
+
+def test_player2_wins():
+    assert get_winning_player(SCISSORS, ROCK) == 2
 
 
