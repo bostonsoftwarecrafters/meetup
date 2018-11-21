1ec3fea728af1bf3175077e18bbede4eac7c550e
ethanstrominger <32078396+ethanstrominger@users.noreply.github.com>
2018-11-21 18:32:27 -0500

    Changed order of words

--- test/test_game.py
+++ test/test_game.py
@@ -1,8 +1,5 @@
 from src.game import get_winning_player, PAPER, SCISSORS, ROCK
 
-def test_pass():
-    assert 1==1
-
 def test_player1_wins():
     assert get_winning_player(ROCK, SCISSORS) == 1
     assert get_winning_player(SCISSORS, PAPER) == 1
