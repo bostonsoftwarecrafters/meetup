e2b7a09b788615340c226eed3821e0464818014f
ethanstrominger <32078396+ethanstrominger@users.noreply.github.com>
2018-12-16 11:13:23 -0500

    Test another commit

--- test/test_game.py
+++ test/test_game.py
@@ -4,7 +4,6 @@ def test_player1_wins():
     assert get_winning_player(ROCK, SCISSORS) == 1
     assert get_winning_player(SCISSORS, PAPER) == 1
 
-
 def test_player2_wins():
     assert get_winning_player(SCISSORS, ROCK) == 2
     assert get_winning_player(ROCK,PAPER) == 2
