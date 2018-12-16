6d8967299fb95cdf18af694a0e4ab8a9fad8c11d
ethanstrominger <32078396+ethanstrominger@users.noreply.github.com>
2018-12-16 11:07:25 -0500

    Test push

--- test/test_game.py
+++ test/test_game.py
@@ -4,6 +4,7 @@ def test_player1_wins():
     assert get_winning_player(ROCK, SCISSORS) == 1
     assert get_winning_player(SCISSORS, PAPER) == 1
 
+
 def test_player2_wins():
     assert get_winning_player(SCISSORS, ROCK) == 2
     assert get_winning_player(ROCK,PAPER) == 2
