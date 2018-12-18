038b76baf9eeae2d1fd8337a113d38bca2bd96d8
ethanstrominger <32078396+ethanstrominger@users.noreply.github.com>
2018-12-17 19:31:17 -0500

    added 2nd 3-player test - or player 2 test

--- src/game.py
+++ src/game.py
@@ -7,7 +7,16 @@ def get_winning_player(player1_hand, player2_hand, player3_hand = None):
     if player3_hand == None:
         return get_two_player_winner(player1_hand, player2_hand)
     else:
-        return 0
+        player1v2 = get_two_player_winner(player1_hand, player2_hand)
+        player1v3 = get_two_player_winner(player1_hand, player3_hand)
+        player2v3 = get_two_player_winner(player2_hand, player3_hand)
+        #if (get_two_player_winner(player1_hand, player2_hand))
+        if (player1_hand, player2_hand) == (PAPER, SCISSORS):
+            return 2
+        elif player2_hand == SCISSORS:
+            return 1
+        else:
+            return 0
 
 
 def get_two_player_winner(player1_hand, player2_hand):
--- test/test_game.py
+++ test/test_game.py
@@ -16,3 +16,9 @@ def test_two_way_tie():
 
 def test_three_way_tie():
     assert 0 == get_winning_player(ROCK, PAPER, SCISSORS)
+
+def test_player1_of_3_wins():
+    assert 1 == get_winning_player(ROCK, SCISSORS, SCISSORS)
+
+def test_player2_of_3_wins():
+    assert 2 == get_winning_player(PAPER, SCISSORS, PAPER)
