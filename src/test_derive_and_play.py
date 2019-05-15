dee4685c3814201a9215770e10bd6fe37051a840
ethanstrominger <ethanstrominger2@gmail.com>
2019-05-14 22:48:35 -0400

    Added a test for picking up rope and magic arrow.

--- src/test_derive_and_play.py
+++ src/test_derive_and_play.py
@@ -1,5 +1,5 @@
-from dnd_constants import MAGIC_ARROW_STR, ROPE_STR, DRAGON_STR
-from game_direction_class import NORTH, EAST
+from dnd_constants import MAGIC_ARROW_STR, ROPE_STR, DRAGON_STR, PIT_STR
+from game_direction_class import NORTH, EAST, WEST
 from mock_game_class import MockGame
 from mock_game_utility import derive_mock_game
 
@@ -18,7 +18,37 @@ def test_mock_move_to_dragon(safe_mock_game_g3_setup_teardown):
 
 def test_mock_move_to_pit(safe_mock_game_g3_setup_teardown):
     game_orig = safe_mock_game_g3_setup_teardown
-    assert_move_to_obj(game_orig, DRAGON_STR)
+    assert_move_to_obj(game_orig, PIT_STR)
+
+def test_pick_up_rope_go_to_pit(safe_mock_game_g3_setup_teardown):
+    game_orig = safe_mock_game_g3_setup_teardown
+    action = game_orig.set_mock_object_location(ROPE_STR, "F5")
+    action = game_orig.set_mock_object_location(PIT_STR, "F6")
+    action = game_orig.do_action_move(EAST, "Go to G4")
+    action = game_orig.do_action_move(EAST, "Go to G5")
+    action = game_orig.do_action_move(NORTH, "Go to F5 pick up rope")
+    action = game_orig.do_action_move(EAST, "Go to F6 to pit")
+    action = game_orig.do_action_move(WEST, "Go to F5 with rope")
+    assert action.result.location == "F5"
+    action = game_orig.do_action_move(EAST, "Go to F6 to pit")
+    assert action.result.location == "F6"
+    action = game_orig.do_action_move(NORTH,"Fail to climb out of pit")
+    assert action.result.location == "F6"
+    game_derived: MockGame = derive_mock_game(game_orig)
+    assert_play_game_same_results(game_derived, game_orig)
+
+def test_pick_up_rope_and_arrow(safe_mock_game_g3_setup_teardown):
+    game_orig = safe_mock_game_g3_setup_teardown
+    action = game_orig.set_mock_object_location(ROPE_STR, "F5")
+    action = game_orig.set_mock_object_location(MAGIC_ARROW_STR, "F6")
+    action = game_orig.do_action_move(EAST, "Go to G4")
+    action = game_orig.do_action_move(EAST, "Go to G5")
+    action = game_orig.do_action_move(NORTH, "Go to F5 pick up rope")
+    action = game_orig.do_action_move(EAST, "Go to F6 to pick up rope")
+    action = game_orig.do_action_move(NORTH, "Go to E5")
+    assert action.result.inventory == [ROPE_STR,MAGIC_ARROW_STR]
+    game_derived: MockGame = derive_mock_game(game_orig)
+    assert_play_game_same_results(game_derived, game_orig)
 
 def assert_move_to_obj(game_orig, obj_str):
     game_orig.set_mock_object_location(obj_str, "F5")
@@ -28,6 +58,10 @@ def assert_move_to_obj(game_orig, obj_str):
     game_orig.do_action_move(EAST, "Go to F6")
     game_derived: MockGame = derive_mock_game(game_orig)
     assert game_derived.get_mock_object_location(obj_str) == ["F5"]
+    assert_play_game_same_results(game_derived, game_orig)
+
+
+def assert_play_game_same_results(game_derived, game_orig):
     orig_action_objs = game_orig.get_actions()
     for action_index in range(1, len(orig_action_objs)):
         action = game_orig.get_action(action_index)
